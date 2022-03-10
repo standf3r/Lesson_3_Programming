528b09ada497c51c24a83be1d41ca9d2f78c191c
standfer <vertigl@yandex.ru>
2022-03-10 22:29:13 +0800

    Homework 2: Число в куб

---
+++ Homework_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Число в куб 
+
+Console.WriteLine("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= c; i++)
+{
+    Console.Write($"{i}: {i * i * i}");
+    Console.WriteLine();
+}
