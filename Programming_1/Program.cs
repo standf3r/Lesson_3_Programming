b428b3f627f9af32313a0b70020e4279efa47862
standfer <vertigl@yandex.ru>
2022-03-10 20:39:10 +0800

    Task 1: x y

--- Programming_1/Program.cs
+++ Programming_1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напиши программу которая принимает на вход координаты точки (x и y), при чем x != 0, y != 0
+// и выдает номер четверти плоскости, в котрой находится эта точка
+
+Console.WriteLine("Введите значение X, не равное нулю: ");
+int NumberX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Y, не равное нулю: ");
+int NumberY = Convert.ToInt32(Console.ReadLine());
