4d05a0d69d3ae16cfc1066cde7ee1e60b14feaef
standfer <vertigl@yandex.ru>
2022-03-10 21:25:31 +0800

    Task 2: Show Quarter

---
+++ Programming_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 18. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти: ");
+int chet = Convert.ToInt32(Console.ReadLine());
+
+int X = Convert.ToInt32(new Random().Next(1, 100));
+int Y = Convert.ToInt32(new Random().Next(-100, -1));
+
+if (chet == 1)
+{
+    Console.WriteLine($"Случайные точки х = {X}, y = {X}");
+}
+if (chet == 2)
+{
+    Console.WriteLine($"Случайные точки x = {Y} , y = {X}");
+}
+if (chet == 3)
+{
+    Console.WriteLine($"Случайные точки x = {Y} , y = {Y}");
+}
+if (chet == 4)
+{
+    Console.WriteLine($"Случайные точки x = {X} , y = {Y}");
+}
+
+
