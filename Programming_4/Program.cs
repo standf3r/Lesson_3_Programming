be5b36ad6d060a0e519e9e930f215ffda72d3317
standfer <vertigl@yandex.ru>
2022-03-10 22:12:17 +0800

    Task 4: Table

---
+++ Programming_4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= c; i++)
+{
+    Console.Write($"{i}: {i * i}");
+    Console.WriteLine();
+}
